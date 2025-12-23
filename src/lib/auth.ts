0c99561c3b996988537de0badd14712469211b94
Samuel <samuelvvelzen@icloud.com>
2025-12-24 00:43:51 +0100

    disable cookie cache

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -200,8 +200,7 @@ export const auth = betterAuth({
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24, // 1 day
     cookieCache: {
-      enabled: true,
-      maxAge: 60 * 60 * 24 * 7, // 7 days
+      enabled: false,
     },
   },
 
