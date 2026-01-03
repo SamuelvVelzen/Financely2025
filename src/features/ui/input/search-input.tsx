afa163908c7b62a4455b5e5f1e0f4bd54bccd71d
Samuel <samuelvvelzen@icloud.com>
2026-01-04 00:57:52 +0100

    improve searchinput

--- src/features/ui/input/search-input.tsx
+++ src/features/ui/input/search-input.tsx
@@ -3,7 +3,7 @@
 import { IconButton } from "@/features/ui/button/icon-button";
 import { cn } from "@/features/util/cn";
 import { IPropsWithClassName } from "@/features/util/type-helpers/props";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useFormContext } from "react-hook-form";
 import { HiX } from "react-icons/hi";
 import { HiMagnifyingGlass } from "react-icons/hi2";
@@ -38,47 +38,67 @@ export function SearchInput({
     setIsCollapsed(isEmpty && !isHovered);
   }, [isEmpty, isHovered]);
 
+  const measureRef = useRef<HTMLSpanElement>(null);
+
+  const [expandedWidth, setExpandedWidth] = useState(300);
+
+  useEffect(() => {
+    if (measureRef.current) {
+      // Measure the placeholder text width
+      const width = measureRef.current.offsetWidth;
+      // Add padding for icons (pl-9 + pr-9 = 72px) + some extra buffer
+      setExpandedWidth(Math.max(300, width + 80));
+    }
+  }, [placeholder]);
+
   return (
-    <div
-      data-collapsed={isCollapsed}
-      data-empty={isEmpty}
-      data-open={isOpen}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className={cn(
-        "relative transition-all duration-300 ease-in-out",
-        "w-9 hover:w-[300px] focus-within:w-[300px]",
-        "has-[input:not(:placeholder-shown)]:w-[300px]",
-        "data-[open=true]:w-[300px]",
-        "data-[collapsed=true]:[&_input]:pr-2!",
-        "data-[collapsed=true]:[&_input]:pl-2!",
-        "data-[open=true]:[&_input]:pl-9!",
-        "data-[open=true]:[&_input]:pr-9!",
-        "focus-within:[&_input]:pl-9!",
-        "focus-within:[&_input]:pr-9!",
-        "data-[collapsed=true]:[&>div>div>div:last-child]:hidden",
-        "focus-within:[&>div>div>div:last-child]:flex!",
-        "[&_input::placeholder]:opacity-0",
-        "hover:[&_input::placeholder]:opacity-100",
-        "focus-within:[&_input::placeholder]:opacity-100",
-        "has-[input:not(:placeholder-shown)]:[&_input::placeholder]:opacity-100",
-        className
-      )}>
-      <TextInput
-        name={name}
-        label={label}
-        placeholder={placeholder}
-        className="truncate"
-        prefixIcon={<HiMagnifyingGlass className="size-5" />}
-        suffixIcon={
-          <IconButton
-            clicked={handleClear}
-            aria-label="Clear search"
-            className="p-0">
-            <HiX className="size-5" />
-          </IconButton>
+    <>
+      <span
+        ref={measureRef}
+        className="absolute invisible whitespace-nowrap text-base"
+        style={{ font: "inherit" }}>
+        {placeholder}
+      </span>
+      <div
+        data-collapsed={isCollapsed}
+        data-empty={isEmpty}
+        data-open={isOpen}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        style={
+          {
+            "--expanded-width": `${expandedWidth}px`,
+          } as React.CSSProperties
         }
-      />
-    </div>
+        className={cn(
+          "relative transition-all ease-in-out",
+          "w-28 hover:w-(--expanded-width) focus-within:w-(--expanded-width)",
+          "has-[input:not(:placeholder-shown)]:w-(--expanded-width)",
+          "data-[open=true]:w-(--expanded-width)",
+          "data-[collapsed=true]:[&_input]:pr-2!",
+          "data-[open=true]:[&_input]:pl-9!",
+          "data-[open=true]:[&_input]:pr-9!",
+          "focus-within:[&_input]:pl-9!",
+          "focus-within:[&_input]:pr-9!",
+          "data-[collapsed=true]:[&>div>div>div:last-child]:hidden",
+          className
+        )}>
+        <TextInput
+          name={name}
+          label={label}
+          placeholder={placeholder}
+          className="truncate"
+          prefixIcon={<HiMagnifyingGlass className="size-5" />}
+          suffixIcon={
+            <IconButton
+              clicked={handleClear}
+              aria-label="Clear search"
+              className="p-0">
+              <HiX className="size-5" />
+            </IconButton>
+          }
+        />
+      </div>
+    </>
   );
 }
