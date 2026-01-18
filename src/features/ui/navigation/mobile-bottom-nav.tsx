9f7d3f037eef661123708953dd56a58ef6e8e613
Samuel <samuelvvelzen@icloud.com>
2026-01-18 17:29:08 +0100

    small improvement to the mobile bottom nav

--- src/features/ui/navigation/mobile-bottom-nav.tsx
+++ src/features/ui/navigation/mobile-bottom-nav.tsx
@@ -25,7 +25,6 @@ export function MobileBottomNav() {
   return (
     <>
       <Container
-        as="nav"
         className={cn(
           "md:hidden mb-0 flex items-stretch",
           "fixed bottom-0 left-0 right-0 z-50",
@@ -37,39 +36,40 @@ export function MobileBottomNav() {
           paddingBottom: "max(1rem, env(safe-area-inset-bottom))",
         }}
         aria-label="Bottom navigation">
-        <MobileNavItem
-          to={ROUTES.ROOT}
-          label="Dashboard"
-          icon={HiChartBar}
-          className="flex-1"
-        />
-        <MobileNavItem
-          to={ROUTES.BUDGETS}
-          label="Budgets"
-          icon={HiOutlineCurrencyEuro}
-          className="flex-1"
-        />
-        <div className="flex items-center justify-center max-w-15 flex-1 px-2 relative">
-          <IconButton
-            clicked={handleCreateTransaction}
-            variant="primary"
-            className={cn("size-14 absolute -top-7 m-0 p-0")}
-            aria-label="Add transaction">
-            <HiPlus className="size-7" />
-          </IconButton>
-        </div>
-        <MobileNavItem
-          to={ROUTES.TRANSACTIONS}
-          label="Transactions"
-          icon={HiArrowsRightLeft}
-          className="flex-1"
-        />
-        <MobileNavItem
-          to={ROUTES.TAGS}
-          label="Tags"
-          icon={HiOutlineTag}
-          className="flex-1"
-        />
+        <nav className="relative flex w-full">
+          <MobileNavItem
+            to={ROUTES.ROOT}
+            label="Dashboard"
+            icon={HiChartBar}
+            className="flex-1"
+          />
+          <MobileNavItem
+            to={ROUTES.BUDGETS}
+            label="Budgets"
+            icon={HiOutlineCurrencyEuro}
+            className="flex-1"
+          />
+          <div className="flex items-center justify-center max-w-15 flex-1 px-2 relative -top-7 m-0 p-0">
+            <IconButton
+              clicked={handleCreateTransaction}
+              variant="primary"
+              aria-label="Add transaction">
+              <HiPlus className="size-10" />
+            </IconButton>
+          </div>
+          <MobileNavItem
+            to={ROUTES.TRANSACTIONS}
+            label="Transactions"
+            icon={HiArrowsRightLeft}
+            className="flex-1"
+          />
+          <MobileNavItem
+            to={ROUTES.TAGS}
+            label="Tags"
+            icon={HiOutlineTag}
+            className="flex-1"
+          />
+        </nav>
       </Container>
       <AddOrCreateTransactionDialog
         open={isTransactionDialogOpen}
