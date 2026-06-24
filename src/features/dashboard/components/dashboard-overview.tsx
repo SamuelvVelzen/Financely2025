dbeffeead1ef775c402214f3665f0e99f360fa18
Samuel <samuelvvelzen@icloud.com>
2026-06-25 00:07:12 +0200

    add dashboard icon

--- src/features/dashboard/components/dashboard-overview.tsx
+++ src/features/dashboard/components/dashboard-overview.tsx
@@ -2,6 +2,7 @@ import { BudgetSummaryCards } from "@/features/budget/components/overview/budget
 import { useBudgetsOverview } from "@/features/budget/hooks/useBudgets";
 import { Container } from "@/features/ui/container/container";
 import { Title } from "@/features/ui/typography/title";
+import { HiOutlineChartBar } from "react-icons/hi2";
 
 export function DashboardOverview() {
   const {
@@ -13,7 +14,9 @@ export function DashboardOverview() {
   return (
     <>
       <Container>
-        <Title>Dashboard</Title>
+        <Title className="flex items-center gap-2">
+          <HiOutlineChartBar />
+          Dashboard</Title>
         <p className="text-text-muted text-sm">
           Welcome to Financely. Manage your finances with ease.
         </p>
